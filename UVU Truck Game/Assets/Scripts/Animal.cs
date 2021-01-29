dcab4edef2feee8960e7625dea4cacf337376651
Anthony Romrell <anthonyromrell@mac.com>
2021-01-29 11:29:54 -0700

    inheritance

---
+++ UVU Truck Game/Assets/Scripts/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    public Color animalColor = Color.red;
+    public float speed = 1.0f;
+    public string animalName = "Bob";
+    public int health = 10;
+
+    private void Start()
+    {
+        Debug.Log("Hello World.");
+    }
+}
---
+++ UVU Truck Game/Assets/Scripts/Practice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice : MonoBehaviour
+{
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "John";
+        Debug.Log(name);  
+    }
+}
+
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        string name = "John";
+        Debug.Log(name);  
+    }
+}
