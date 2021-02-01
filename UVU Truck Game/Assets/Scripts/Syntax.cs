d259858ca5737508458195394b7a32695027eacd
Anthony Romrell <anthonyromrell@mac.com>
2021-02-01 10:49:10 -0700

    User Inputs

--- UVU Truck Game/Assets/Scripts/Syntax.cs
+++ UVU Truck Game/Assets/Scripts/Syntax.cs
@@ -1,22 +1,10 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Syntax : MonoBehaviour
+﻿public class Syntax
 {
-    void Update()
+    public int number = 2;
+
+    public void Run()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            GetComponent<Renderer> ().material.color = Color.red;
-        }
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            GetComponent<Renderer>().material.color = Color.green;
-        }
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            GetComponent<Renderer>().material.color = Color.blue;
-        }
+        
     }
 }
 
