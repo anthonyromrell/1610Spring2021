51aab9e9ab30183b3afc0a11fca64deca123a631
Anthony Romrell <anthonyromrell@mac.com>
2021-02-05 10:41:38 -0700

    User Input Updates

--- UVU Truck Game/Assets/Scripts/UserInput.cs
+++ UVU Truck Game/Assets/Scripts/UserInput.cs
@@ -1,23 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class UserInput : MonoBehaviour
-{
-    public Rigidbody2D ballRigidbody2D;
+{ 
+    public Rigidbody2D ballRigidbody2D; 
     public float speed = 3f;
-    public float jumpForce = 300f;
+    public float jumpForce = 20f;
     
     private Vector2 direction;
     private Vector2 yDirection;
-    
-    void Update()
+
+    private int score;
+
+    private void Update()
     {
-        direction.x = Input.GetAxis("Horizontal") * speed;
-        ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
-        
-        if (Input.GetButtonDown("Jump"))
-        {
-            yDirection.y = jumpForce;
-            ballRigidbody2D.AddForce(yDirection, ForceMode2D.Force);
-        }
+        direction.x = Input.GetAxis("Horizontal");
+        if (!Input.GetButtonDown("Jump")) return;
+        yDirection.y = jumpForce;
+    }
+
+    private void FixedUpdate()
+    {
+        var newDirection = direction * (speed * Time.deltaTime);
+        ballRigidbody2D.AddForce(newDirection, ForceMode2D.Force);
+        ballRigidbody2D.AddForce(yDirection, ForceMode2D.Force);
     }
 }
