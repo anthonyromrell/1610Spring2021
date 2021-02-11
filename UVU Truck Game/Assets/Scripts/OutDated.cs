77328adf669d10eef2677eda193747fc51c1de42
Anthony Romrell <anthonyromrell@mac.com>
2021-02-11 14:51:47 -0700

    IntData Scriptable Objects

---
+++ UVU Truck Game/Assets/Scripts/OutDated.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class OutDated : MonoBehaviour
+{
+    public Text myText; //Use this in place of GUIText
+    public Image myTexture; //Use this is place of GUITexture
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
